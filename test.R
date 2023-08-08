f0bb5e7f40956f456fbbffb2148f833ebbe48479
王乙庭 <wangyiting@wytMacBook-Air.hitronhub.home>
2023-08-08 22:37:25 +0800

    Add test.R

--- test.R
+++ test.R
@@ -1,7 +1,7 @@
 ### Modeling Part
 
 ## read data for training
-df = read.csv("/Users/wangyiting/Desktop/English/作品集/data.csv")
+df = read.csv("data.csv")
 dim(df)
 head(df)
 
@@ -20,7 +20,7 @@ df$Time[hour == 12 | hour == 17] = "ClassEnd"
 head(df)
 
 ## read data for testing
-df2 = read.csv("/Users/wangyiting/Desktop/English/作品集/data2.csv")
+df2 = read.csv("data2.csv")
 dim(df2)
 head(df2)
 
@@ -54,10 +54,6 @@ df$Time = relevel(factor(df$Time), ref = "Other")
 glm = glm(Sufficient ~ Day + Time + AirPressure + AirTemperature + RelativeHumidity + WindSpeed + Precipitation + SunshineDuration, data = df)
 summary(glm)
 
-## robust coef
-library(AER)
-coeftest(glm, vcov. = vcovHC, type = "HC0")
-
 ## selection
 library(leaps)
 best.fit = regsubsets(Sufficient ~ ., data = df[,c(4:12)], nvmax = 16)
@@ -80,31 +76,6 @@ summary(m2)
 
 m3 = glm(Sufficient ~ Time + AirPressure + RelativeHumidity + WindSpeed, data = df, family = binomial())
 summary(m3)
-
-## robust coef
-library(AER)
-coeftest(m1, vcov. = vcovHC, type = "HC0")
-
-## prediction (not robust)
-p1 = predict(m1, newdata = data.frame("Time" = c("ClassStart"), "AirPressure" = c(1011.0), "RelativeHumidity" = c(80), "WindSpeed" = c(3.4)), type = "response") # 5/9 9:00
-p2 = predict(m1, newdata = data.frame("Time" = c("Other"), "AirPressure" = c(1010.2), "RelativeHumidity" = c(72), "WindSpeed" = c(2.6)), type = "response") # 5/9 13:00
-p3 = predict(m1, newdata = data.frame("Time" = c("ClassEnd"), "AirPressure" = c(1008.4), "RelativeHumidity" = c(82), "WindSpeed" = c(2.9)), type = "response") # 5/9 17:00
-
-## prediction (robust)
-beta = coeftest(m1, vcov. = vcovHC, type = "HC0")[1:6]
-xnew1 = c(1, 0, 1, 1011.0 , 80, 3.4) # 5/9 9:00
-xnew2 = c(1, 0, 0, 1010.2 , 72, 2.6) # 5/9 13:00
-xnew3 = c(1, 1, 0, 1008.4 , 82, 2.9) # 5/9 17:00
-
-log_odds1 = t(beta) %*% xnew1
-log_odds2 = t(beta) %*% xnew2
-log_odds3 = t(beta) %*% xnew3
-
-p1 = exp(log_odds1) / (1 + exp(log_odds1)) 
-p2 = exp(log_odds2) / (1 + exp(log_odds2)) 
-p3 = exp(log_odds3) / (1 + exp(log_odds3)) 
-
-c(p1, p2, p3)
 
 ## testing
 
